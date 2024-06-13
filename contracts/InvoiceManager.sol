cbe4939ecd15230a075661ad4429a35640b89b0f
Alex Gartner <git@agartner.com>
2024-06-13 02:30:26 -0700

    swap

--- contracts/InvoiceManager.sol
+++ contracts/InvoiceManager.sol
@@ -4,9 +4,17 @@ pragma solidity 0.8.7;
 import "@zetachain/protocol-contracts/contracts/zevm/SystemContract.sol";
 import "@zetachain/protocol-contracts/contracts/zevm/interfaces/zContract.sol";
 import "@zetachain/toolkit/contracts/OnlySystem.sol";
+import "@zetachain/toolkit/contracts/SwapHelperLib.sol";
+import "@zetachain/toolkit/contracts/BytesHelperLib.sol";
+import "@pythnetwork/pyth-sdk-solidity/IPyth.sol";
+import "@pythnetwork/pyth-sdk-solidity/PythStructs.sol";
 
 contract InvoiceManager is zContract, OnlySystem {
     SystemContract public systemContract;
+
+    IPyth pyth;
+    
+    mapping(address => bytes32) public zrc20ToPythId;
 
 	struct Invoice {
         uint id;
@@ -19,14 +27,23 @@ contract InvoiceManager is zContract, OnlySystem {
 	Invoice[] public invoices;
     uint public nextInvoiceId;
 
+    address usdcEthAddress;
+
 	// Mapping from creator address to an array of invoice IDs
     mapping(address => uint[]) public invoicesByCreator;
 
     event InvoiceCreated(uint id, address creator, string description, uint256 priceUSD);
     event InvoicePaid(uint id, address payer);
 
-    constructor(address systemContractAddress) {
-        systemContract = SystemContract(systemContractAddress);
+    constructor(address _systemContractAddress, address _pythContract, address _usdcEthAddress) {
+        systemContract = SystemContract(_systemContractAddress);
+        pyth = IPyth(_pythContract);
+        usdcEthAddress = _usdcEthAddress;
+
+        // eth testnet
+        zrc20ToPythId[address(0x05BA149A7bd6dC1F937fA9046A9e05C05f3b18b0)] = 0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace;
+        // bnb.bsc testnet
+        zrc20ToPythId[address(0xd97B1de3619ed2c6BEb3860147E30cA8A7dC9891)] = 0x2f95862b045670cd22bee3114c39763a4a08beeb663b145d283c31d7d1101c4f;
     }
 
     function onCrossChainCall(
@@ -35,7 +52,35 @@ contract InvoiceManager is zContract, OnlySystem {
         uint256 amount,
         bytes calldata message
     ) external virtual override onlySystem(systemContract) {
-        // TODO: implement the logic
+        (uint invoiceId) = abi.decode(
+            message,
+            (uint)
+        );
+
+        Invoice storage invoice = invoices[invoiceId];
+ 
+        (address gasZRC20, uint256 gasFee) = IZRC20(usdcEthAddress)
+            .withdrawGasFee();
+ 
+        uint256 inputForGas = SwapHelperLib.swapTokensForExactTokens(
+            systemContract,
+            zrc20,
+            gasFee,
+            gasZRC20,
+            amount
+        );
+ 
+        uint256 outputAmount = SwapHelperLib.swapExactTokensForTokens(
+            systemContract,
+            zrc20,
+            amount - inputForGas,
+            usdcEthAddress,
+            0
+        );
+
+        IZRC20(gasZRC20).approve(usdcEthAddress, gasFee);
+        IZRC20(usdcEthAddress).withdraw(abi.encodePacked(invoice.creator), outputAmount);
+        invoice.paid = true;
     }
 
 	function createInvoice(string memory _description, uint256 _priceUSD) public {
